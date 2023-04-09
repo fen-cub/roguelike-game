3e55903c0fdd8aca65480b78abb16b8d2594120e
Vdaleke <vdalekesmirnov@gmail.com>
2023-04-10 00:32:47 +0300

    update PlayerCharacter source

--- Source/roguelike_game/Character/PlayerCharacter.cpp
+++ Source/roguelike_game/Character/PlayerCharacter.cpp
@@ -19,15 +19,15 @@ APlayerCharacter::APlayerCharacter()
 	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
 
-	GetCapsuleComponent()->InitCapsuleSize(45.0f, 45.0f);
+	GetCapsuleComponent()->InitCapsuleSize(10.0f, 10.0f);
 
-	GetSprite()->SetRelativeRotation(FRotator( 0.0f, 90.0f, -90.0f));
+	GetSprite()->SetRelativeRotation(FRotator(0.0f, 90.0f, -90.0f));
 	GetSprite()->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->SetRelativeRotation(FRotator(-90.0f, 180.0f, 180.0f));
-	CameraBoom->TargetArmLength = 600.0f;
+	CameraBoom->TargetArmLength = 80.0f;
 	CameraBoom->bInheritPitch = false;
 	CameraBoom->bInheritYaw = false;
 	CameraBoom->bInheritRoll = false;
@@ -35,23 +35,38 @@ APlayerCharacter::APlayerCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>("Follow Camera");
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
-	
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
-	
+
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+	GetCharacterMovement()->MaxWalkSpeed = 100.0f;
+
 	bDead = false;
+	bIsMoving = false;
 }
 
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	OnCharacterMovementUpdated.AddDynamic(this, &APlayerCharacter::Animate);
+}
+
+void APlayerCharacter::AddMovementInput(FVector WorldDirection, float ScaleValue, bool bForce)
+{
+	auto MovementComponent = GetMovementComponent();
+
+	if (MovementComponent)
+	{
+		MovementComponent->AddInputVector(WorldDirection * ScaleValue, bForce);
+	}
+	else
+	{
+		Internal_AddMovementInput(WorldDirection * ScaleValue, bForce);
+	}
 }
 
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	// выполнение дополнительных действий в Tick()
 }
 
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -62,15 +77,93 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
 }
 
+void APlayerCharacter::SetCurrentAnimationDirection(FVector const& Velocity)
+{
+	const float x = Velocity.GetSafeNormal().X;
+	const float y = Velocity.GetSafeNormal().Y;
+
+	bIsMoving = x != 0.0f || y != 0.0f;
+
+	if (bIsMoving)
+	{
+		if (y > 0.5f)
+		{
+			CurrentAnimationDirection = EAnimationDirection::Right;
+			GetSprite()->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+		}
+		else if (y < -0.5f)
+		{
+			CurrentAnimationDirection = EAnimationDirection::Left;
+			GetSprite()->SetRelativeScale3D(FVector(-0.5f, 0.5f, 0.5f));
+		}
+		else if (x < -0.5f)
+		{
+			CurrentAnimationDirection = EAnimationDirection::Down;
+			GetSprite()->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+		}
+		else if (x > 0.5f)
+		{
+			CurrentAnimationDirection = EAnimationDirection::Up;
+			GetSprite()->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+		}
+	}
+}
+
+void APlayerCharacter::Animate(float DeltaTime, FVector OldLocation, FVector const OldVelocity)
+{
+	SetCurrentAnimationDirection(OldVelocity);
+
+	if (OldVelocity.Size() > 0.0f)
+	{
+		switch (CurrentAnimationDirection)
+		{
+		case EAnimationDirection::Up:
+			GetSprite()->SetFlipbook(Flipbooks.WalkUp);
+			break;
+		case EAnimationDirection::Down:
+			GetSprite()->SetFlipbook(Flipbooks.WalkDown);
+			break;
+		case EAnimationDirection::Left:
+			GetSprite()->SetFlipbook(Flipbooks.WalkRight);
+			break;
+		case EAnimationDirection::Right:
+			GetSprite()->SetFlipbook(Flipbooks.WalkRight);
+			break;
+		default:
+			break;
+		}
+	}
+	else
+	{
+		switch (CurrentAnimationDirection)
+		{
+		case EAnimationDirection::Up:
+			GetSprite()->SetFlipbook(Flipbooks.IdleUp);
+			break;
+		case EAnimationDirection::Down:
+			GetSprite()->SetFlipbook(Flipbooks.IdleDown);
+			break;
+		case EAnimationDirection::Left:
+			GetSprite()->SetFlipbook(Flipbooks.IdleRight);
+			break;
+		case EAnimationDirection::Right:
+			GetSprite()->SetFlipbook(Flipbooks.IdleRight);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
 void APlayerCharacter::MoveForward(float Axis)
 {
 	if ((Controller != nullptr) && (Axis != 0) && !bDead)
 	{
-		const FRotator Rotation = Controller ->GetControlRotation();
+		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Axis);
+		AddMovementInput(Direction, Axis, true);
 	}
 }
 
@@ -78,10 +171,10 @@ void APlayerCharacter::MoveRight(float Axis)
 {
 	if ((Controller != nullptr) && (Axis != 0) && !bDead)
 	{
-		const FRotator Rotation = Controller ->GetControlRotation();
+		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		AddMovementInput(Direction, Axis);
+		AddMovementInput(Direction, Axis, true);
 	}
 }
--- Source/roguelike_game/Character/PlayerCharacter.h
+++ Source/roguelike_game/Character/PlayerCharacter.h
@@ -9,6 +9,40 @@
 /**
  * 
  */
+
+UENUM(BlueprintType, Category = "AnimationCharacter|Animation")
+enum class EAnimationDirection : uint8
+{
+	Up,
+	Down,
+	Left,
+	Right
+};
+
+USTRUCT(BlueprintType, Category = "Animation")
+struct FAnimationFlipbooks
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* IdleUp{nullptr};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* IdleRight{nullptr};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* IdleDown{nullptr};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* WalkRight{nullptr};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* WalkUp{nullptr};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* WalkDown{nullptr};
+};
+
 UCLASS()
 class ROGUELIKE_GAME_API APlayerCharacter : public APaperCharacter
 {
@@ -17,24 +51,40 @@ class ROGUELIKE_GAME_API APlayerCharacter : public APaperCharacter
 public:
 	APlayerCharacter();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bDead;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void AddMovementInput(FVector WorldDirection, float ScaleValue, bool bForce) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable, Category="AnimationCharacter|Animation")
+	void SetCurrentAnimationDirection(FVector const& Velocity);
+
+	UFUNCTION(BlueprintCallable, Category="AnimationCharacter|Animation")
+	void Animate(float DeltaTime, FVector OldLocation, FVector const OldVelocity);
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	class USpringArmComponent* CameraBoom;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	class UCameraComponent* FollowCamera;
-	
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool bDead;
-	
-protected:
-	virtual void BeginPlay() override;
-	
-public:
-	virtual void Tick(float DeltaSeconds) override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimationCharacter|Config")
+	EAnimationDirection CurrentAnimationDirection{EAnimationDirection::Down};
 
-	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimationCharacter|Config")
+	FAnimationFlipbooks Flipbooks;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimationCharacter|Config")
+	uint8 bIsMoving : 1;
 
 	void MoveForward(float Axis);
 	void MoveRight(float Axis);
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
 };
