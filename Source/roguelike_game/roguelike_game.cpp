aef634b95727e1a600c16645d9ae5647c9066400
Vdaleke <vdalekesmirnov@gmail.com>
2023-04-09 17:24:54 +0300

    init unreal project

---
+++ Source/roguelike_game/roguelike_game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "roguelike_game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, roguelike_game, "roguelike_game" );
---
+++ Source/roguelike_game/roguelike_gameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "roguelike_gameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKE_GAME_API Aroguelike_gameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
